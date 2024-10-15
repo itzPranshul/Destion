2ed16fee0c345e1a9f2e85365bdffd0de0d34335
pranshul gupta <pranshulg01@gmail.com>
2024-10-15 09:24:25 +0530

    initializing project

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './Login'
+import Dashboard from './Dashboard'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Login/> */}
+    {/* <Dashboard/> */}
   </React.StrictMode>
 );
 
